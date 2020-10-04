1e1c56a69dc67f0a5afaf421e66f390b13329ac0
Leandro Rodrigues <leandro.rodrigues.silva99@gmail.com>
2020-10-04 12:37:44 -0300

    Add fake_quote command

--- bot.py
+++ bot.py
@@ -4,13 +4,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, \
                          Filters
 from telegram import User
 from dotenv import load_dotenv
+from validator_collection import checkers
 
 import logging
 import shlex
 import subprocess
 import random
 import os
-
+import requests
 
 load_dotenv()
 TOKEN = os.getenv("CITADOR_TOKEN")
@@ -26,37 +27,93 @@ def start(bot, update):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Olá! Use o comando /quote em resposta a uma mensagem')
 
+def format_name(name):
+    return name[1].upper() + ", " + name[0] if len(name) > 1 else name[0]
+
 def get_user_pic(user: User):
     name = user.first_name + " " + user.last_name if user.last_name else user.first_name
     name = name.split(" ")
-    formated_name = name[1].upper() + ", " + name[0] if len(name) > 1 else name[0]
+    formatted_name = format_name(name)
 
     photos = user.get_profile_photos().photos
 
     if not photos or not photos[0]:
-        return formated_name
+        return formatted_name
 
     # first photo, last version (better quality)
     photo_size = photos[0][-1]
     photo = photo_size.get_file().download_as_bytearray()
 
-    return (photo, (photo_size.width, photo_size.height), formated_name)
+    return (photo, (photo_size.width, photo_size.height), formatted_name)
+
+def get_pic_from_url(url, update):
+    if not checkers.is_url(url):
+        update.message.reply_text('Use um URL válido')
+        return None
+
+    response = requests.get(url)
+    valid_content_types = ['image/jpeg', 'image/png']
+
+    if not response.headers['content-type'] in valid_content_types:
+        update.message.reply_text('O URL deve ser de uma imagem jpeg ou png.')
+        return None
 
 
-def apply_overlay(n, photo, quote, name):
+    image = response.content
+
+    return (image, (500, 500))
+
+def apply_overlay(n, photo, quote, name, fake_quote=False):
     result = f'result-{n}.jpg'
 
-    if photo:
+    if fake_quote:
+        script = 'make_image_fake_quote.sh'
+        fake_mark = "Esta é uma falsa citação gerada com /fake_quote"
+        command = ['bash', script, quote, name, photo, fake_mark, result]
+    elif photo:
         script = 'make_image.sh'
+        command = ['bash', script, quote, name, photo, result]
     else:
         script = 'make_image_noprofile.sh'
         photo = 'None'
+        command = ['bash', script, quote, name, photo, result]
 
-    command = ['bash', script, quote, name, photo, result]
     subprocess.call(command)
 
     return result
 
+def make_fake_quote(bot, update):
+    print('fake_quote ' + update.effective_user.username)
+    splitted_text = update['message']['text'].split()
+
+    if len(splitted_text) < 5:
+        update.message.reply_text('Use /fake_quote <url da imagem> <nome> <sobrenome> <frase>.')
+        return
+
+    url = splitted_text[1]
+    photo = get_pic_from_url(url, update)
+
+    if photo is None:
+        return
+
+    fake_quote = True
+    name = [splitted_text[2], splitted_text[3]]
+    quote = splitted_text[4:]
+    joined_quote = ' '.join(quote)
+    formatted_name = format_name(name)
+
+    n = random.randint(1, 1000)
+    file_name = False
+
+    file_name = f'image-{n}.jpg'
+    open(file_name, 'wb').write(photo[0])
+    result = apply_overlay(n, file_name, joined_quote, formatted_name, fake_quote)
+
+    update.message.reply_photo(photo=open(result, 'rb'))
+
+    if file_name:
+        os.remove(file_name)
+    os.remove(result)
 
 def make_quote(bot, update):
     print('quote ' + update.effective_user.username)
@@ -112,6 +169,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("quote", make_quote))
+    dp.add_handler(CommandHandler("fake_quote", make_fake_quote))
     dp.add_handler(MessageHandler(Filters.forwarded, make_quote))
 
     # log all errors
